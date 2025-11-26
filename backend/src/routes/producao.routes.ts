348e62f1d942fc5e3961305613e1496410fbf0f0
Enzo Gabriel <enzokof2005@gmail.com>
2025-11-26 13:09:30 -0300

    Corrigir rotas de producao com nomes corretos do controller

--- backend/src/routes/producao.routes.ts
+++ backend/src/routes/producao.routes.ts
@@ -1,17 +1,13 @@
-import { Router } from 'express';
-import * as ProducaoController from '../controllers/producao.controller';
+import { Router } from 'express'
+import * as ProducaoController from '../controllers/producao.controller'
 
-const router = Router();
+const router = Router()
 
-router.get('/', ProducaoController.listarProducoes);
-router.get('/:id', ProducaoController.buscarProducaoPorId);
-router.get('/aeronave/:aeronaveId', ProducaoController.buscarProducaoPorAeronave);
-router.get('/estatisticas', ProducaoController.obterEstatisticasProducao);
-router.post('/', ProducaoController.criarProducao);
-router.put('/:id', ProducaoController.atualizarProducao);
-router.put('/:id/percentual', ProducaoController.atualizarPercentualProducao);
-router.put('/:id/finalizar', ProducaoController.finalizarProducao);
-router.put('/:id/cancelar', ProducaoController.cancelarProducao);
-router.delete('/:id', ProducaoController.deletarProducao);
+router.get('/', ProducaoController.listarProducoes)
+router.get('/:id', ProducaoController.buscarProducaoPorId)
+router.get('/aeronave/:aeronaveId', ProducaoController.buscarProducaoPorAeronave)
+router.post('/', ProducaoController.criarProducao)
+router.put('/:id', ProducaoController.atualizarProducao)
+router.put('/:id/percentual', ProducaoController.atualizarPercentualProducao)
 
-export default router;
+export default router
